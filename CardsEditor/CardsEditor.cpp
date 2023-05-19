ed270b1f0f4d47f54f480a5486fccb8e580cef72
Alexander Termer <termeralexandr@gmail.com>
2023-05-19 16:03:09 +0300

    setup imgui and glfw

---
+++ CardsEditor/CardsEditor.cpp
@@ -0,0 +1,18 @@
+
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <stdio.h>
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hi";
+
+	return 0;
+}
